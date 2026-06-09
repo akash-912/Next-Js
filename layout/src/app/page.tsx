9393c0951c24e86584b4e94e62f7a3fe5870b72c
akash-912 <akashdeep12896@gmail.com>
2026-06-09 22:59:41 +0530

    layout.tsx

---
+++ layout/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+const Home = () => {
+  return (
+    <div>
+      <Link href="/login">Login page</Link>
+    </div>
+  )
+}
+
+export default Home
